6cc14cf9bbd7a2e7009e7add268b590aa2acbf28
Anthony Guidotti <anthony@aguidotti.com>
2020-12-29 18:47:07 -0500

    Add x and bb hotkeys and add hotkey help modal

--- src/main/html/App.svelte
+++ src/main/html/App.svelte
@@ -21,6 +21,7 @@
     let activeNote;
     let activeQuality;
     let activeWaveform = 'sawtooth';
+    let showHelpModal = false;
 
     jQuery.ajax('/notes/TWELVE_440.json').done(retrievedNotes => {
         notesMap = retrievedNotes.notes;
@@ -43,14 +44,15 @@
                     if (['A', 'B', 'C', 'D', 'E', 'F', 'G'].includes(key)) {
                         // Remove enharmonic, makes it possible to set natural
                         // note when enharmonic set
-                        activeNoteStore.set(activeNote.replace(/[#b]/, '').replace(/[A-G]/, key));
+                        activeNoteStore.set(activeNote.replace(/[#bx]+/, '').replace(/[A-G]/, key));
                     }
-                    if (['#', '@'].includes(key)) {
-                        if (key === '#' && ['C', 'D', 'F', 'G', 'A'].includes(activeNote[0]) ||
-                            key === '@' && ['D', 'E', 'G', 'A', 'B'].includes(activeNote[0])) {
-                            key = key.replace('@', 'b');
-                            if (activeNote.includes('#') || activeNote.includes('b')) {
-                                activeNoteStore.set(activeNote.replace(/[#b]/, key));
+                    if (['#', '@', 'X', '!'].includes(key)) {
+                        if (key === '#' || key === '@' ||
+                            (key === 'X' && ['C', 'D', 'F', 'G', 'A'].includes(activeNote[0])) ||
+                            (key === '!' && ['D', 'E', 'G', 'A', 'B'].includes(activeNote[0]))) {
+                            key = key.replace('@', 'b').replace('!', 'bb').replace('X', 'x');
+                            if (activeNote.includes('#') || activeNote.includes('b') || activeNote.includes('x')) {
+                                activeNoteStore.set(activeNote.replace(/[#bx]+/, key));
                             } else {
                                 activeNoteStore.set(activeNote.slice(0, 1) + key + activeNote.slice(1));
                             }
@@ -61,10 +63,11 @@
                             activeNoteStore.set(activeNote.replace(/[-\d]+$/, key));
                         }
                     })
+                    console.debug(activeNote);
                 }
             }
         });
-    })
+    })()
 
     function createOscillators() {
         if (activeQuality) {
@@ -96,7 +99,9 @@
 </script>
 
 <style>
-    @import "webjars/bootstrap/4.5.3/css/bootstrap.css";
+    :global(.cursor-pointer) {
+        cursor: pointer;
+    }
 </style>
 
 <nav class="d-flex w-100 position-fixed justify-content-between align-items-center px-2" style="height:3rem;top:0;">
@@ -124,11 +129,45 @@
         {/if}
     </div>
     <div class="d-flex align-items-center justify-content-end">
-
+        <i class="fas fa-question-circle text-dark mr-2 cursor-pointer"
+           style="font-size:1.3rem;"
+           on:click={() => showHelpModal = true}></i>
     </div>
 </nav>
+
 <div class="p-3 mt-5 flex-grow-1">
     <div class="d-flex align-items-center">
 
     </div>
 </div>
+
+{#if showHelpModal}
+<div class="position-absolute w-100 h-100 d-flex p-3" style="background:rgba(0,0,0,0.3);">
+    <div class="flex-grow-1 bg-white p-4">
+        <div class="d-flex justify-content-between w-100">
+            <h4>Help</h4>
+            <i class="fas fa-times-circle text-dark cursor-pointer"
+               style="font-size:1.3rem;"
+               on:click={() => showHelpModal = false}></i>
+        </div>
+        <h5>Keyboard Shortcuts:</h5>
+        <table class="table table-sm table-striped w-50">
+            <tbody>
+            {#each [
+                ['a, b, c, d, e, f', 'Change active note'],
+                ['0, 1, 2, 3, 4, 5, 6, 7, 8, 9', 'Change active octave'],
+                ['#', 'Sharp active note'],
+                ['@', 'Flat active note (b)'],
+                ['x', 'Double sharp active note'],
+                ['!', 'Double flat active note (bb)']
+            ] as instruction}
+                <tr>
+                    <td class="font-weight-bold">{instruction[0]}</td>
+                    <td>{instruction[1]}</td>
+                </tr>
+            {/each}
+            </tbody>
+        </table>
+    </div>
+</div>
+{/if}
